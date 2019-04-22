b6400e1e3c4defa7d920eaee93e9ff15001d6f87
emily1oo15 <eu0903203@otc.edu>
2019-04-22 11:33:20 -0500

    case1-4
    case1-4

--- homework/case1-4/case1/sd_mapper.js
+++ homework/case1-4/case1/sd_mapper.js
@@ -10,9 +10,13 @@
 */
 
 "use strict";
-var thisTime = new Date("February 3, 2018 3:15:28");
+
+/*thisTime section: time and date above picture*/
+var thisTime = new Date("April 22, 2019 11:32:52");
 var timeStr = thisTime.toLocaleString();
 document.getElementById("timeStamp").innerHTML = timeStr;
+
+/*working on the map to make sure it puts the correct part of it on the page*/
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
 var mapNum= (2*thisMonth + thisHour)%24;
